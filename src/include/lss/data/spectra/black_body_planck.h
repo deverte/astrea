05a2756a49bdc247ba3a02a422f2b0137c181abd
deverte <deverte@ya.ru>
2023-04-20 17:22:10 +0700

    2023-04-20

--- src/include/lss/data/spectra/black_body_planck.h
+++ src/include/lss/data/spectra/black_body_planck.h
@@ -4,8 +4,6 @@
 #include <cmath>
 #include <limits>
 
-#include <boost/math/quadrature/trapezoidal.hpp>
-
 #include "./spectrum.h"
 #include "../../physics/constants.h"
 
@@ -43,75 +41,49 @@ BlackBodyPlanck::operator()(double wavelength /* nm */) {
   auto& c = SPEED_OF_LIGHT; // cm * s^{-1}
   auto& h = PLANCK_CONSTANT; // eV * s
   auto& k_B = BOLTZMANN_CONSTANT; // eV * K^{-1}
+  auto& pi = PI; // 1
 
   auto cm_to_nm = 10000000.0;
   auto eV_to_J = 1.602177e-19;
   auto nm_to_m = 1.0e-9;
 
-  auto F = total_area_; // W * m^{-2}
-  auto B_lambda_tot = total_area_temperature_; // W * m^{-2}
+  auto E_e = total_area_; // W * m^{-2}
+  auto J = total_area_temperature_; // W * m^{-2}
   auto lambda = wavelength; // nm
   auto lambda_0 = min_wavelength_; // nm
   auto lambda_infty = max_wavelength_; // nm
   auto T = temperature_; // K
 
-  auto B = [&](double lambda, double T) {
+  auto R_lambda = [&](double lambda, double T) {
     return // J * s^{-1} * m^{-2} * nm^{-1} = W * m^{-2} * nm^{-1}
-      + 2.0 * h * eV_to_J * std::pow(c * cm_to_nm, 2.0) // J * nm^2 * s^{-1}
+      + 2.0 * pi * h * eV_to_J * std::pow(c * cm_to_nm, 2.0) // J * nm^2 * s^{-1}
       / std::pow(lambda, 5.0)                           // nm^{-5}
       / (std::exp(h * c * cm_to_nm / (lambda * k_B * T)) - 1.0) // 1
       * std::pow(nm_to_m, -2.0)
     ;
   };
 
-  auto E = [&](double lambda, double T) {
-    return F * B(lambda, T) / B_lambda_tot;
+  auto E_e_lambda = [&](double lambda, double T) {
+    return E_e * R_lambda(lambda, T) / J;
   };
 
-  return E(lambda, T);
+  return E_e_lambda(lambda, T);
 }
 
 
 inline void BlackBodyPlanck::temperature(double value /* K */) {
   temperature_ = value;
 
-  auto& c = SPEED_OF_LIGHT; // cm * s^{-1}
-  auto& h = PLANCK_CONSTANT; // eV * s
-  auto& k_B = BOLTZMANN_CONSTANT; // eV * K^{-1}
-
-  auto cm_to_nm = 10000000.0;
-  auto eV_to_J = 1.602177e-19;
-  auto nm_to_m = 1.0e-9;
-
-  auto lambda_0 = min_wavelength_; // nm
-  auto lambda_infty = max_wavelength_; // nm
+  auto sigma = 5.67040040e-8; // W * m^{-2} * K^{-4}
   auto T = temperature_; // K
 
-  auto B = [&](double lambda, double T) {
-    return // J * s^{-1} * m^{-2} * nm^{-1} = W * m^{-2} * nm^{-1}
-      + 2.0 * h * eV_to_J * std::pow(c * cm_to_nm, 2.0) // J * nm^2 * s^{-1}
-      / std::pow(lambda, 5.0)                           // nm^{-5}
-      / (std::exp(h * c * cm_to_nm / (lambda * k_B * T)) - 1.0) // 1
-      * std::pow(nm_to_m, -2.0)
-    ;
-  };
+  double J = sigma * std::pow(T, 4.0); // W * m^{-2}
 
-  double B_lambda_tot = boost::math::quadrature::trapezoidal( // W * m^{-2}
-    [&](double lambda /* nm */) {
-      return
-        + B(lambda, T) // W * m^{-2} * nm^{-1}
-        // * dlambda   // nm
-      ;
-    },
-    lambda_0,
-    lambda_infty
-  );
-
-  total_area_temperature_ = B_lambda_tot;
+  total_area_temperature_ = J;
 }
 
 
-inline void BlackBodyPlanck::total_area(double value /* W * M^{-2} */) {
+inline void BlackBodyPlanck::total_area(double value /* W * m^{-2} */) {
   total_area_ = value;
 }
 
